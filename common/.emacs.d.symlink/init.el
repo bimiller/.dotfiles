4d2abc3865f3885801ea8841b66dfdc5db0c08da
Bill Miller <bill02.miller@sap.com>
2015-09-28 12:32:43 -0400

    initial linux changes

--- common/.emacs.d.symlink/init.el
+++ common/.emacs.d.symlink/init.el
@@ -139,7 +139,7 @@
     ("Inconsolata" . ,(if (eq system-type 'darwin) 140 110))
     ("Menlo" . 130)
     ("Consolas" . 120)
-    ("DejaVu Sans Mono" . 110)
+    ("DejaVu Sans Mono" . 130)
     ("Courier New" . 130))
   "My preferred monospace fonts.
 The `car' of each item is the font family, the `cdr' the preferred font size.")
@@ -191,7 +191,7 @@ VAR-VAL should be a (VAR VAL) pair."
 ;; Use UTF-8 by default for new files, file detection, etc.
 (prefer-coding-system 'utf-8)
 
-;; I map this to the Cygwin tmp directory on MS Windows.
+;; I map this to the system tmp directory.
 (setq temporary-file-directory "/tmp/")
 
 ;; Keep backup and auto save files out of the way.
@@ -763,13 +763,8 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-safe-themes (quote ("146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" default)))
- '(fci-rule-color "#383838")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
+ )
+ (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
